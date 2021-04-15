a01bbef30b2d71c198006ea76a8491d85cd395c2
Arthur <arthur.sarry@gmail.com>
2021-04-15 15:36:35 +0200

    Update install.sh and add Konsole config

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,3 +19,14 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # Install a few linters with pip3
 pip3 install pylint cpplint
+
+# Install R and RStudio packages
+sudo pacman -S r
+sudo pamac build rstudio-desktop-bin
+
+# Install LaTeX packages
+sudo pacman -S texlive-most texlive-bin
+
+# Install Oh-My-Zsh and Powerlevel10k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
